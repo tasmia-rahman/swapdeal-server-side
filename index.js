fdb116fc5a3c9e1a2a73aea10fea90a37fcffaac
tasmia-rahman-maliha <maliha1288@gmail.com>
2022-11-25 21:32:46 +0600

    categories set

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require('express');
 var cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middle wares
 app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.aw5zzia.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+        const categoriesCollection = client.db('swapdealDB').collection('categories');
+
+        //Categories
+        app.get('/categories', async (req, res) => {
+            const query = {}
+            const cursor = categoriesCollection.find(query);
+            const categories = await cursor.toArray();
+            res.send(categories);
+        });
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('API running');
